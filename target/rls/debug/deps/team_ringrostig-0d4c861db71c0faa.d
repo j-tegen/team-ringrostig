063ff664483ca4de9933a5434e539b034725fa4f
Jonatan Tegen <jonatan.tegen@gmail.com>
2019-09-19 09:53:04 +0200

    Add gitignore

--- target/rls/debug/deps/team_ringrostig-0d4c861db71c0faa.d
+++
@@ -1,5 +0,0 @@
-/Users/jonatan/Documents/team-ringrostig/target/rls/debug/deps/team_ringrostig-0d4c861db71c0faa.rmeta: src/main.rs
-
-/Users/jonatan/Documents/team-ringrostig/target/rls/debug/deps/team_ringrostig-0d4c861db71c0faa.d: src/main.rs
-
-src/main.rs:
--- target/rls/debug/deps/team_ringrostig-6e7688a5fdcd73ba.d
+++
@@ -1,5 +0,0 @@
-/Users/jonatan/Documents/team-ringrostig/target/rls/debug/deps/team_ringrostig-6e7688a5fdcd73ba.rmeta: src/main.rs
-
-/Users/jonatan/Documents/team-ringrostig/target/rls/debug/deps/team_ringrostig-6e7688a5fdcd73ba.d: src/main.rs
-
-src/main.rs:
