b7846c2c6cc1721bc6e3dc2d143ef93e4b3d993c
christofferflystam <christoffer.flystam@getaccept.com>
2019-09-19 15:52:23 +0200

    add fruit of the day

--- src/graphql_schema.rs
+++ src/graphql_schema.rs
@@ -8,6 +8,18 @@ struct Fruit {
     video_thumb: String,
 }
 
+struct User {
+    id: i32,
+    name: String,
+}
+
+struct FruitOfTheDay {
+    id: i32,
+    user_id: i32,
+    fruit_id: i32,
+    time_stamp: String,
+}
+
 #[juniper::object(description = "A fruit!")]
 impl Fruit {
     pub fn id(&self) -> i32 {
@@ -31,6 +43,42 @@ impl Fruit {
     }
 }
 
+#[juniper::object(description = "A user!")]
+impl User {
+    pub fn id(&self) -> i32 {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+}
+
+#[juniper::object(description = "A fruit of the day!")]
+impl FruitOfTheDay {
+    pub fn id(&self) -> i32 {
+        self.id
+    }
+
+    pub fn user(&self) -> User {
+        get_users()
+            .into_iter()
+            .find(|user| user.id == self.user_id)
+            .unwrap()
+    }
+
+    pub fn time_stamp(&self) -> &str {
+        self.time_stamp.as_str()
+    }
+
+    pub fn fruit(&self) -> Fruit {
+        all_fruits()
+            .into_iter()
+            .find(|fruit| fruit.id == self.fruit_id)
+            .unwrap()
+    }
+}
+
 pub struct QueryRoot;
 
 #[juniper::object]
@@ -44,6 +92,10 @@ impl QueryRoot {
             .into_iter()
             .find(|fruit| fruit.id == id)
             .unwrap()
+    }
+
+    fn fruitsOfTheDay() -> Vec<FruitOfTheDay> {
+        all_fruits_of_the_day()
     }
 }
 
@@ -68,6 +120,32 @@ fn all_fruits() -> Vec<Fruit> {
             description: "Orange in swedisih literally means chinese apple!".to_owned(),
             video_url: "https://www.youtube.com/watch?v=ZN5PoW7_kdA".to_owned(),
             video_thumb: "https://i.ytimg.com/an_webp/ZN5PoW7_kdA/mqdefault_6s.webp?du=3000&sqp=CICLjewF&rs=AOn4CLD6fMuupHGmNwKcCvbfQOKR0hYwuA".to_owned(),
+        },
+    ]
+}
+
+fn all_fruits_of_the_day() -> Vec<FruitOfTheDay> {
+    vec![FruitOfTheDay {
+        id: 1,
+        user_id: 2,
+        time_stamp: "jajaja".to_owned(),
+        fruit_id: 2,
+    }]
+}
+
+fn get_users() -> Vec<User> {
+    vec![
+        User {
+            id: 1,
+            name: "Kalle".to_owned(),
+        },
+        User {
+            id: 2,
+            name: "Ramona".to_owned(),
+        },
+        User {
+            id: 3,
+            name: "Jonte".to_owned(),
         },
     ]
 }
