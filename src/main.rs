d2e2742dd08bfb68ab7f67c9d43135942f6d5072
Jonatan Tegen <jonatan.tegen@gmail.com>
2019-09-19 12:18:29 +0200

    Set host and port from env variables

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate actix_web;
 extern crate futures;
 extern crate juniper;
 
+use std::env;
 use std::io;
 use std::sync::Arc;
 
@@ -16,13 +17,14 @@ use crate::graphql_schema::{create_schema, Schema};
 
 fn main() -> io::Result<()> {
     let schema = std::sync::Arc::new(create_schema());
+    let url = get_base_url();
     HttpServer::new(move || {
         App::new()
             .data(schema.clone())
             .service(web::resource("/graphql").route(web::post().to_async(graphql)))
             .service(web::resource("/graphiql").route(web::get().to(graphiql)))
     })
-    .bind("localhost:3003")?
+    .bind(url)?
     .run()
 }
 
@@ -43,8 +45,19 @@ fn graphql(
 }
 
 fn graphiql() -> HttpResponse {
-    let html = graphiql_source("http://localhost:3003");
+    let url = get_base_url().to_owned() + "/graphql";
+    let html = graphiql_source(url.as_str());
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html)
+}
+
+fn get_env_string<'a>(key: &'a str, default: &'a str) -> String {
+    env::var(key).unwrap_or_else(|_error| default.to_owned())
+}
+
+fn get_base_url() -> String {
+    let host = get_env_string("HOST", "0.0.0.0");
+    let port = get_env_string("PORT", "3003");
+    host + ":" + port.as_str()
 }
